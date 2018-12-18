b903ceb4c35c7f7ce50054d6913e1dfdf45dbf37
caprenter <caprenter@gmail.com>
2018-12-18 10:37:52 +0000

    Turns duplicate routines into functions

--- CleanUp.py
+++ CleanUp.py
@@ -17,23 +17,35 @@ Usage:
 
 # Set some variables
 basepath = '/home/david/Desktop/python_fun/Audio'
+delete_dir = '/delete/' #NB needs slashes either side
 #files_to_keep = ['Audwall', 'MYRD_SYS'] # Note used yet. NB we also want to keep files that start with "PSQ"
 # A tuple of file extensions that we want to keep. all of the below are legit
 extensions_to_keep = ('.WAV', '.wav', '.lst', '.LST', '.ini', '.INI', '.pk', '.RTF', '.RT')
+
+def ourFiles (basepath):
+	files = []
+	for fname in os.listdir(basepath):
+		path = os.path.join(basepath, fname)
+		if os.path.isdir(path):
+		   # skip directories
+			continue
+		else:
+			files.append(fname)
+
+	return (files)
+	
+def moveFiles (files, basepath, delete_dir):
+	for file in files:
+		os.rename(basepath + '/' + file, basepath + delete_dir + file)
+		print("Moving " + file)
+
 
 ### First task is to remove any files that should not be in there by extension e.g jpg, m4a, mpg, whatevs ###
 ### Second task is to filter out bad WAV files by filename ###
 
 ## Task 1 ##
 #Get all the files we want to check - make sure we skip directories
-OurFiles = [] #listdir(dir)
-for fname in os.listdir(basepath):
-    path = os.path.join(basepath, fname)
-    if os.path.isdir(path):
-        # skip directories
-        continue
-    else:
-		OurFiles.append(fname)
+OurFiles = ourFiles (basepath)
 
 #Find the bad files we don't want to be there
 badFiles =[]
@@ -45,28 +57,18 @@ for file in OurFiles:
 print(badFiles)
 
 # Move the bad files to the 'delete' directory
-for file in badFiles:
-	os.rename(basepath + '/' + file, basepath + "/delete/" + file)
-	print("Moving " + file)
+moveFiles (badFiles, basepath, delete_dir)
 	
 ## Task 2 ##
 # Second task is to filter out bad WAV files by filename
 
-# Check only the files with these extensions
-wav_ex = ('.WAV', '.wav')
-
 # Fetach all the wav files
-wavs = [] #listdir(dir)
-for fname in os.listdir(basepath):
-    path = os.path.join(basepath, fname)
-    if os.path.isdir(path):
-       # skip directories
-        continue
-    else:
-		wavs.append(fname)
+wavs = ourFiles (basepath)
 
 # Weed out the bad wavs
 badwavs =[]
+# Check only the files with these extensions
+wav_ex = ('.WAV', '.wav')
 
 for file in wavs:
 	if file.endswith(wav_ex): #let WAV and wav through
@@ -78,6 +80,7 @@ for file in wavs:
 print(badwavs)
 
 # Move the bad wavs to the 'delete' directory
-for file in badwavs:
-	os.rename(basepath + '/' + file, basepath + "/delete/" + file)
-	print("Moving " + file)
+moveFiles (badwavs, basepath, delete_dir)
+
+	
+	
