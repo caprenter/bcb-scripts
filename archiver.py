b9fe138e97d1c7be5c93fe27fdd00a2e7384de5f
caprenter <caprenter@gmail.com>
2019-01-09 18:33:21 +0000

    Updates the script to creat a useful .mp3 filename
    Made with a specific case in mind, so future versions may improve on this.

--- archiver.py
+++ archiver.py
@@ -4,6 +4,8 @@ from os import listdir
 from os.path import isfile, join, splitext
 import string
 import re
+import time
+import datetime
 """
 Syed and David are trying to loop over a bunch of Myriad Transport files (.zip)
 to extract the data from the .LST files, turn the .WAVs into mp3s with
@@ -24,7 +26,12 @@ run
   python archiver.py
   
 """
-	
+##Set some variables
+# mp3 data
+fileprefix = "BradfordRocks" #for filename
+artist = "BCB Radio 106.6FM" # for artist tag
+album = "Bradford Rocks"
+
 #Get the files we want to extract
 OurFiles = listdir("files")
 
@@ -55,7 +62,7 @@ for file in extractedFiles:
 print(listFiles)
 
 splitter = "      | - "
-
+i=1
 #Loop over the list files to read and process the data we need for the mp3 tags
 for file in listFiles:
 	notes = open("extracted/" + file, "r")
@@ -75,14 +82,21 @@ for file in listFiles:
 	#While we have the .LST file, we generate the name of the corresponding wav file
 	wavfile = splitext("extracted/" + file)[0] + ".WAV"
 	
-	#Create the mp3
+	## Create the mp3
 	title = notes[0] #use the 'first' line of the .LST file
-	artist = "BCB Radio 106.6FM"
+	#mp3 filename - created from a prefix set above (i.e. show name), a counter, a timestamp and .mp3 
+	ts = time.time()
+	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+	mp3filename = fileprefix + "_" + str(i) + "_" + st + ".mp3"
+	print(mp3filename)
+		
+	
 	notes.pop(0) #removes the first item from the list which we have already used for the title
 	comment = ', '.join(notes) #uses rest of list items text for the comment
-	cmd = 'lame --preset standard --tt "{}" --ta "{}" --tc "{}" {} '.format(title, artist, comment, wavfile)
+	#Run the command to make the mp3 using lame
+	cmd = 'lame --preset standard --tt "{}" --tl "{}" --ta "{}" --tc "{}" {} {} '.format(title, album, artist, comment, wavfile, mp3filename) #wavfile is input, mp3filename is output
 	subprocess.call(cmd, shell=True)
-
+	i +=1
 
 #for filename in OurFiles:
 #    print '%20s  %s' % (filename, zipfile.is_zipfile(filename))
